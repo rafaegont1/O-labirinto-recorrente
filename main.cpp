ef868e601d2edf3075dcd29ed4ccf4ee62543193
rafaegont1 <109547317+rafaegont1@users.noreply.github.com>
2023-03-28 10:16:19 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+typedef struct boy {
+  unsigned short x, y, lives, items;
+  boy() : x(0), y(0), lives(10), items(0) {}
+} Boy;
+
+void joystick(unsigned short *restrict mat, unsigned short *restrict i, unsigned short *restrict j) {
+}
+
+int main(int argc, char *argv[]) {
+  struct boy;
+  unsigned short *mat, mat_sz;
+
+  while(life != 0) {
+  }
+
+  return 0;
+}
